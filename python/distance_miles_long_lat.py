27e95f8e1f5c2a3edcb881d41121b8416910f86d
Jeff McGrath <jeff@youtah.com>
2015-01-13 01:09:13 -0700

    Changed it to handle four arguments, creating two lists. No error or exception handling, yet.

--- python/distance_miles_long_lat.py
+++ python/distance_miles_long_lat.py
@@ -1,13 +1,18 @@
-
 #!/usr/bin/env python
 import math as m
+import sys
  
-def main():
+def main(argv):
     # Creating two lists as points
-    saltlake = [40.7665, -111.912]
-    miamihotel = [25.81708, -80.12088]
- 
-    print distance(miamihotel,saltlake)
+    # Example args for Salt Lake to Miami would be:
+    # 40.7665 -111.912 25.81708 -80.12088
+    # As the args, with 25.81708, -80.12088 being for Miami
+    # and 40.7665 -111.912 being for Salt Lake City.
+    
+    pointa = [float(argv[1]), float(argv[2])]
+    pointb = [float(argv[3]), float(argv[4])]
+
+    print distance(pointa, pointb)
  
 #To calculate the distance between two points (lat & long) - result is in miles (as the crow flies.)
 def distance(p1, p2):
@@ -20,4 +25,4 @@ def distance(p1, p2):
     return d
  
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
