d32c59d48af8d23102b9e6354262d779485d8fa5
Jeff McGrath <jeff@youtah.com>
2015-01-15 14:25:31 -0700

    Adding McKinley into the array

--- perl/change_calculator_exercise.pl
+++ perl/change_calculator_exercise.pl
@@ -1,9 +1,9 @@
 #!/usr/bin/env perl
- 
+
 use strict;
 use warnings;
 use feature 'say';
- 
+
 my $money = shift;
 my @billz = ( 100000, 10000, 5000, 1000, 100, 50, 20, 10, 5, 2, 1 );
 my %prezz = (
@@ -11,6 +11,7 @@ my %prezz = (
                 10000   => 'Chase',
                 5000    => 'Madison',
                 1000    => 'Cleveland',
+                500     => 'McKinley',
                 100     => 'Franklin',
                 50      => 'Grant',
                 20      => 'Jackson',
@@ -19,9 +20,9 @@ my %prezz = (
                 2       => 'Jefferson',
                 1       => 'Washington'
             );
- 
+
 say "\$$money in the fewest number of bills would be: ";
- 
+
 for (my $index=0; $index<=$#billz; $index++ ) {
     my $current_bill = $billz[$index];
     my $divided = int($money / $current_bill);
@@ -36,4 +37,3 @@ for (my $index=0; $index<=$#billz; $index++ ) {
         say "There is $divided $current_bill_prez.";
     }
 }
-
