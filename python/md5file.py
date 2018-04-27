39693d3d3cd6ce55bb60195cab4e23a747b93429
Jeff McGrath <jeff@youtah.com>
2018-04-27 11:32:56 -0600

    Update md5file.py

--- python/md5file.py
+++ python/md5file.py
@@ -1,1 +1,36 @@
+#!/usr/bin/env python
+import argparse
+import hashlib
+import sys
+import os
 
+def main():
+    args = parse_args()
+    filename = args.filename
+    if os.path.isfile(filename):
+        try:
+            filedata = open(filename,'rb').read()
+            md5hash = hashlib.md5(filedata).hexdigest()
+            print("{0} is the md5 hash for file '{1}'".format(md5hash, filename))
+        except Exception as e:
+            print("Error: an exception occurred. \n{0}".format(str(e)))
+            raise e
+    else:
+        print("Error: file '{0}' does not exist.".format(filename))
+        sys.exit(1)
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+                        '-f',
+                        '--filename',
+                        type=str,
+                        required=True,
+                        help='Path to the filename. Example: /tmp/example.iso'
+                    )
+    args = parser.parse_args()
+    return args
+
+if __name__ == "__main__":
+    main()
+~             
